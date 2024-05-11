e50476e873838f78dd7c25ad1710960272e89a10
Davide Marincione <76567598+Davide-F5-Marincione@users.noreply.github.com>
2024-05-11 19:44:19 +0200

    gestures work?!

--- gesture_code.py
+++ gesture_code.py
@@ -1,105 +1,80 @@
 import cv2 as cv
 import numpy as np
 import mediapipe as mp
-from typing import NamedTuple
 from threading import Thread
 from collections import deque
-import matplotlib.pyplot as plt
 
 from timeit import default_timer as timer
 
-class HandData(NamedTuple):
-    abs_hand: np.ndarray
-    origin: np.ndarray
-    basis: np.ndarray
-    norm_hand: np.ndarray
-    handedness: bool
-    palm_width: float
+class Hand:
 
-def normalize_hand(hand_repr):
-    abs_hand = np.array([(landmark.x, landmark.y, landmark.z) for landmark in hand_repr.hand_landmarks[0]])
+    def __init__(self, hand_repr, timestamp_ms, prev_click=False):
+        abs_hand = np.array([(landmark.x, landmark.y, landmark.z) for landmark in hand_repr.hand_landmarks[0]])
 
-    palm = abs_hand[0] * .5 + abs_hand[5] * .125 + abs_hand[9] * .125 + abs_hand[13] * .125 + abs_hand[17] * .125 # Center of palm
+        palm = abs_hand[0] * .5 + abs_hand[5] * .125 + abs_hand[9] * .125 + abs_hand[13] * .125 + abs_hand[17] * .125 # Center of palm
 
-    palm_width = np.linalg.norm(abs_hand[5] - abs_hand[17]) # Distance between index and pinky base
-    rel_hand = (abs_hand - palm) / palm_width # Normalize to palm
+        palm_width = np.linalg.norm(abs_hand[5] - abs_hand[17]) # Distance between index and pinky base
+        rel_hand = (abs_hand - palm) / palm_width # Normalize to palm
 
-    wrist = rel_hand[0]
-    index_base = rel_hand[5]
-    middle_base = rel_hand[9]
-    pinky_base = rel_hand[17]
+        wrist = rel_hand[0]
+        index_base = rel_hand[5]
+        middle_base = rel_hand[9]
+        pinky_base = rel_hand[17]
 
-    palm_normal = np.cross(index_base - wrist, pinky_base - wrist)
-    palm_normal = palm_normal / np.linalg.norm(palm_normal)
+        palm_normal = np.cross(index_base - wrist, pinky_base - wrist)
+        palm_normal = palm_normal / np.linalg.norm(palm_normal)
+        
+        if hand_repr.handedness[0][0].category_name == 'Left':
+            palm_normal = -palm_normal
+
+        pinky_normal = np.cross(palm_normal, middle_base - wrist)
+        pinky_normal = pinky_normal / np.linalg.norm(pinky_normal)
+
+        fingers_normal = np.cross(pinky_normal, palm_normal)
+        fingers_normal = fingers_normal / np.linalg.norm(fingers_normal)
+
+        if hand_repr.handedness[0][0].category_name == 'Left':
+            pinky_normal = -pinky_normal
+
+        basis = np.array([pinky_normal, fingers_normal, palm_normal]).T
+        norm_hand = rel_hand @ basis
+
+        self.abs_hand = abs_hand
+        self.origin = palm
+        self.basis = basis
+        self.norm_hand = norm_hand
+        self.handedness = hand_repr.handedness[0][0].category_name == 'Right'
+        self.palm_width = palm_width
+        self.timestamp_ms = timestamp_ms
+        self.is_click = self._is_clicking(prev_click)
+
+    def is_same(self, other):
+        if self.handedness != other.handedness:
+            return False
+        
+        if abs(self.palm_width - other.palm_width) > self.palm_width * .1:
+            return False
+        
+        return np.linalg.norm(self.origin - other.origin) < self.palm_width * 10
+
+    def is_facing_screen(self):
+        return self.basis[2, 2] < 0
     
-    if hand_repr.handedness[0][0].category_name == 'Left':
-        palm_normal = -palm_normal
+    def _is_clicking(self, prev_click=False):
 
-    pinky_normal = np.cross(palm_normal, middle_base - wrist)
-    pinky_normal = pinky_normal / np.linalg.norm(pinky_normal)
+        thumb_vec = self.norm_hand[4]
+        index_vec = self.norm_hand[8]
 
-    fingers_normal = np.cross(pinky_normal, palm_normal)
-    fingers_normal = fingers_normal / np.linalg.norm(fingers_normal)
+        thumb_vec = thumb_vec / np.linalg.norm(thumb_vec)
+        index_vec = index_vec / np.linalg.norm(index_vec)
 
-    if hand_repr.handedness[0][0].category_name == 'Left':
-        pinky_normal = -pinky_normal
+        dot = np.dot(thumb_vec, index_vec)
+        dist = np.linalg.norm(thumb_vec - index_vec)
 
-    basis = np.array([pinky_normal, fingers_normal, palm_normal]).T
-    norm_hand = rel_hand @ basis
-
-    return HandData(abs_hand, palm, basis, norm_hand, hand_repr.handedness[0][0].category_name == 'Right', palm_width)
-
-def is_same_hand(hand1, hand2):
-    if hand1.handedness != hand2.handedness:
-        return False
-    
-    if abs(hand1.palm_width - hand2.palm_width) > hand1.palm_width * .1:
-        return False
-    
-    return np.linalg.norm(hand1.origin - hand2.origin) < hand1.palm_width * 10
-
-def is_facing_screen(hand):
-    return hand.basis[2, 2] < 0
-
-def recognize_tap(before, after):
-    dist = np.linalg.norm(before.origin - after.origin)
-    if dist > before.palm_width * .5:
-        return False
-
-    index_after = ((after.abs_hand[8] - after.origin) / before.palm_width) @ before.basis
-    check = index_after[2] > before.norm_hand[8,2] + .15
-
-    return check
-
-
-def recognize_grab(hand):
-
-    diff = (np.linalg.norm(hand.norm_hand[4] - hand.norm_hand[8])
-            + np.linalg.norm(hand.norm_hand[4] - hand.norm_hand[12])
-            + np.linalg.norm(hand.norm_hand[8] - hand.norm_hand[12])) / 3
-    if diff > .6:
-        return False
-
-    thumb_vec = hand.norm_hand[4]
-    index_vec = hand.norm_hand[8]
-    middle_vec = hand.norm_hand[12]
-
-    thumb_vec = thumb_vec / np.linalg.norm(thumb_vec)
-    index_vec = index_vec / np.linalg.norm(index_vec)
-    middle_vec = middle_vec / np.linalg.norm(middle_vec)
-
-    dot1 = np.dot(thumb_vec, index_vec)
-    dot2 = np.dot(thumb_vec, middle_vec)
-    dot3 = np.dot(index_vec, middle_vec)
-
-    if dot1 < .9 or dot2 < .9 or dot3 < .9:
-        return False
-    
-    return True
-
+        return dot >= (.8 if prev_click else .9) and dist <= (.8 if prev_click else .5)
 
 class HandDetector:
-    def __init__(self, model_path='hand_landmarker.task', h_flip=False, delay_ms=200, min_hand_movement=.005):
+    def __init__(self, model_path='hand_landmarker.task', h_flip=False, cursor_speed=.5, delete_gesture_ms=200, end_tracking_ms=700, min_cursor_movement=.01):
         self.h_flip = h_flip
 
         self.cam = cv.VideoCapture(0)
@@ -107,39 +82,49 @@ class HandDetector:
             print("Cannot open camera")
             exit(0)
 
-        # fps_input_stream = int(self.cam.get(5))
-        self.grabbed, self.frame = self.cam.read()
-        if self.grabbed is False:
-            print('No frames to read')
-            exit(0)
-
-        self.screenspace_hand_positions = deque()
-
-        self.stopped = True
+        self.cursor_pos = np.zeros(2, dtype=np.float32)
+        self.min_cursor_movement = min_cursor_movement
+        self.cursor_speed = cursor_speed
+        self.clicks = deque()
+        self.prev_click = False
+        self.end_tracking_ms = end_tracking_ms
+        self.delete_gesture_ms = delete_gesture_ms
+        self.reset = True
+        self.last_timestamp = None
+        self.curr_hand = None
+        self.prev_hand = None
         
         def callback(result: mp.tasks.vision.HandLandmarkerResult, output_image: mp.Image, timestamp_ms: int):
             if len(result.hand_landmarks) > 0:
-                self.screenspace_hand_positions.append((result, timestamp_ms))
+                if self.curr_hand is None:
+                    self.curr_hand = Hand(result, timestamp_ms)
+                else:
+                    self.prev_hand = self.curr_hand
+                    self.curr_hand = Hand(result, timestamp_ms, self.prev_hand.is_click)
+                self.clicks.append((self.curr_hand.is_click, timestamp_ms))
 
         options = mp.tasks.vision.HandLandmarkerOptions(
             base_options=mp.tasks.BaseOptions(model_asset_path=model_path),
             running_mode=mp.tasks.vision.RunningMode.LIVE_STREAM,
-            # min_tracking_confidence=.1,
             result_callback=callback)
         self.hand_landmarker = mp.tasks.vision.HandLandmarker.create_from_options(options)
 
         self.t = Thread(target=self.update, args=())
         self.t.daemon = True
-
-
-        self.hand_pos = np.zeros(2, dtype=np.float32)
-        self.delay_ms = delay_ms
-        self.min_hand_movement = min_hand_movement
-        self.curr_line = None
+        self.stopped = True
 
     def start(self):
         self.stopped = False
+        self.curr_hand = None
+        self.prev_hand = None
+        self.clicks = deque()
+        self.last_timestamp = None
+        self.reset = True
+        self.prev_click = False
         self.t.start()
+
+    def stop(self):
+        self.stopped = True
 
     def update(self):
         while not self.stopped:
@@ -152,53 +137,73 @@ class HandDetector:
             if self.h_flip:
                 self.frame = cv.flip(self.frame, 1)
 
+            self.frame.flags.writeable = False
+
             self.hand_landmarker.detect_async(mp.Image(image_format=mp.ImageFormat.SRGB, data= cv.cvtColor(self.frame, cv.COLOR_BGR2RGB)), int(timer() * 1000))
 
         self.cam.release()
     
     def process_gestures(self, curr_time_ms):
-        transition_time = curr_time_ms - self.delay_ms
+        if self.last_timestamp is None:
+            self.last_timestamp = curr_time_ms
+
+        delta_t = (curr_time_ms - self.last_timestamp) / 1000
+        self.last_timestamp = curr_time_ms
+
+        if self.curr_hand is None:
+            return (None, False, False, -1000)
         
-        if self.curr_line is None:
-            while len(self.screenspace_hand_positions) > 1:
-                prev_det = self.screenspace_hand_positions[0]
-                next_det = self.screenspace_hand_positions[1]
+        if self.reset:
+            self.reset = False
+            self.cursor_pos = self.curr_hand.origin[:2]
+        
+        if not self.prev_hand is None:
+            if not self.prev_hand.is_same(self.curr_hand):
+                pass # Do something? Hands are different!
 
-                if next_det[1] >= transition_time:
-                    hand1 = normalize_hand(prev_det[0])
-                    hand2 = normalize_hand(next_det[0])
+            dist = np.linalg.norm(self.prev_hand.origin[:2] - self.cursor_pos)
+            if dist <= self.min_cursor_movement:
+                self.prev_hand = None
 
-                    if not is_same_hand(hand1, hand2) or not is_facing_screen(hand1) or not is_facing_screen(hand2):
-                        self.screenspace_hand_positions.popleft()
-                        continue
 
-                    self.curr_line = (self.hand_pos, hand1.origin[:2], hand2.origin[:2], transition_time, next_det[1])
-                    print(recognize_tap(hand1, hand2), recognize_grab(hand2))
-                    break
-                
-                self.screenspace_hand_positions.popleft()
+        dist = np.linalg.norm(self.curr_hand.origin[:2] - self.cursor_pos)
+        if self.prev_hand is None:
+            # Linear interpolation
+            if dist > self.min_cursor_movement:
+                perc = min(1, self.cursor_speed * delta_t / dist)
+                self.cursor_pos = np.clip(self.cursor_pos + (self.curr_hand.origin[:2] - self.cursor_pos) * perc ,0,1)
+        else:
+            # Quadratic interpolation
+            if dist > self.min_cursor_movement:
+                perc = min(1, self.cursor_speed * delta_t / dist)
 
-        if not self.curr_line is None:
-            p1 = self.curr_line[0]
-            p2 = self.curr_line[1]
-            new_pos = p3 = self.curr_line[2]
-            t1 = self.curr_line[3]
-            t2 = self.curr_line[4]
+                p1 = self.cursor_pos + (self.prev_hand.origin[:2] - self.cursor_pos) * perc
+                p2 = self.prev_hand.origin[:2] + (self.curr_hand.origin[:2] - self.prev_hand.origin[:2]) * perc
 
-            if transition_time < t2:
-                i = (transition_time - t1) / (t2 - t1)
-                l1 = p1 + (p2 - p1) * i
-                l2 = p2 + (p3 - p2) * i
-                new_pos = l1 + (l2 - l1) * i
-            else:
-                self.curr_line = None
+                self.cursor_pos = np.clip(p1 + (p2 - p1) * perc, 0, 1)
 
-            if np.linalg.norm(new_pos - self.hand_pos) > self.min_hand_movement:
-                self.hand_pos = new_pos
+        # self.curr_hand.return_index_thumb()
 
-            return True
-    
-        return False
+        while len(self.clicks) > 0:
+            click, timestamp = self.clicks[0]
+            if timestamp + self.delete_gesture_ms >= curr_time_ms:
+                break
+            self.clicks.popleft() 
 
-    def stop(self):
-        self.stopped = True
+        is_click = False
+        for click, timestamp in self.clicks:
+            is_click = is_click or click
+
+        release = self.prev_click and not is_click
+        click = not self.prev_click and is_click
+        self.prev_click = is_click
+
+        check = self.curr_hand.timestamp_ms + self.end_tracking_ms >= curr_time_ms
+        if not check:
+            self.curr_hand = None
+            self.prev_hand = None
+            self.click = deque()
+            self.prev_click = False
+            self.reset = True
+            return (None, False, False, -1000)
+        return self.cursor_pos, click, release, self.curr_hand.timestamp_ms
--- gesture_main.py
+++ gesture_main.py
@@ -18,7 +18,8 @@ clicker = objects.Clicker(renderer)
 
 objects.load_consts()
 
-context_text = objects.FloatingText(renderer, objects.Point(300, 400), "No hand detected", 16, cfg.colors["boardtext"])
+context_text = objects.FloatingText(renderer, objects.Point(300, 400), "Mouse tracking", 16, cfg.colors["boardtext"])
+interaction_text = objects.FloatingText(renderer, objects.Point(300, 450), "No interaction", 16, cfg.colors["boardtext"])
 hand_detector = gesture_code.HandDetector(h_flip=True)
 
 # Main loop
@@ -26,23 +27,29 @@ pygame.mouse.set_visible(False)
 running = True
 cursor_pos = objects.Point(0, 0)
 mouse_pos = objects.Point(0, 0)
+mouse_timestamp = -1000
+last_interaction = -1000
 
 hand_detector.start()
 
 while running:
     curr_time = int(timer() * 1000)
     new_mouse_pos = objects.Point(*pygame.mouse.get_pos())
-    is_tracking = hand_detector.process_gestures(curr_time)
-
     if new_mouse_pos != mouse_pos:
         mouse_pos = new_mouse_pos
+        mouse_timestamp = curr_time
+    hand_cursor_pos, hand_click, hand_release, hand_timestamp = hand_detector.process_gestures(curr_time)
+
+    if mouse_timestamp >= hand_timestamp:
         cursor_pos = mouse_pos
+        context_text.set_text("Mouse tracking")
     else:
-        if is_tracking:
-            context_text.set_text("Detecting hand")
-            cursor_pos = objects.Point(int(hand_detector.hand_pos[0] * WIDTH), int(hand_detector.hand_pos[1] * HEIGHT))
-        else:
-            context_text.set_text("No hand detected")
+        cursor_pos = objects.Point(int(hand_cursor_pos[0] * WIDTH), int(hand_cursor_pos[1] * HEIGHT))
+        context_text.set_text("Hand tracking")
+        if hand_click:
+            pygame.event.post(pygame.event.Event(pygame.MOUSEBUTTONDOWN, button=1))
+        if hand_release:
+            pygame.event.post(pygame.event.Event(pygame.MOUSEBUTTONUP, button=1))
     
     clicker.highlight(cursor_pos)
     for event in pygame.event.get():
@@ -52,9 +59,16 @@ while running:
             case pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:
                     clicker.execute_click()
+                    interaction_text.set_text("Click")
+                    last_interaction = curr_time
             case pygame.MOUSEBUTTONUP:
                 if event.button == 1:
                     clicker.execute_click(False)
+                    interaction_text.set_text("Release")
+                    last_interaction = curr_time
+
+    if curr_time - last_interaction > 1000:
+        interaction_text.set_text("No interaction")
                 
     renderer.step(cursor_pos)
 
